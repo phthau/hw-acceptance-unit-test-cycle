7c8d7e0c41c472eb5d3e22f8ec882ab4b3a3ac7b
Phi Thuan Au <ec2-user@ip-172-31-0-149.us-east-2.compute.internal>
2021-03-11 21:39:55 +0000

    Add more specs.

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -3,6 +3,65 @@ require 'support/action_controller_workaround'
 
 describe MoviesController, type: :controller do
   
+  describe 'show home page' do
+    it 'should render the home page' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+  
+  describe 'add movie page' do 
+    it 'should render the #new page' do 
+      get :new
+      expect(response).to render_template(:new)
+    end
+    
+    it 'should add a movie' do
+      params = 
+      { movie: 
+        {
+          title: 'Inception', 
+          rating: 'PG', 
+          director: 'Christopher Nolan', 
+          release_date: '2010-01-01'
+        }
+      }
+      post :create, params
+      expect(response).to redirect_to(movies_path)
+    end
+  end
+  
+  describe 'edit movie' do 
+    it 'should render the #edit page given a movie' do 
+      @movie = Movie.create!(:title => 'Inception')
+      get :edit, {:id => @movie.id}
+      expect(response).to render_template(:edit)
+    end
+    
+    it 'should edit the movie' do 
+      @movie = Movie.create!(:title => 'Inception')
+      params = {
+        movie: {
+          release_date: '2010-01-01'
+        },
+        id: @movie.id
+      }
+      put :update, params
+      
+      # verify update is made in database
+      @movie = Movie.where("id = ?", @movie.id).first
+      expect(@movie[:release_date]).to eq '2010-01-01'
+    end
+  end
+  
+  describe 'show movie details page' do 
+    it 'should render the #show page given a movie' do 
+      @movie = Movie.create!(:title => 'Inception')
+      get :show, {:id => @movie.id}
+      expect(response).to render_template(:show)
+    end
+  end
+  
   describe 'show movies with same director' do
     # set up data for testing
     before :all do
